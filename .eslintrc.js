2d0cbb57645662e67c58ec2f0bd97c4640449dda
Philip Blyth <philip.blyth@autodesk.com>
2021-05-10 22:07:46 -0400

    style: add typescript, eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,88 @@
+const jsExtensions = ".{j,t}s?(x)";
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        // let `import/order` do its thing:
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        ignoreDeclarationSort: true,
+        allowSeparatedGroups: true,
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+        ],
+        pathGroups: [
+          // separate immediate parents into own group:
+          {
+            pattern: "../!(..)**",
+            group: "parent",
+            position: "after",
+          },
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: false,
+        },
+      },
+    ],
+    // 'import/no-cycle': ['error', {}],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          // Jest, Storybook:
+          "**/__{tests,stories,mocks,fixtures}__/**/*" + jsExtensions,
+          "**/*.{{spec,test}?(s),stories}" + jsExtensions,
+          "{,.}{jest,storybook}/**/*" + jsExtensions,
+          // Scripts:
+          "{bin,script}/**/*" + jsExtensions,
+          // Configs:
+          "**/?(*.)config?(.*).{js,json}",
+          "**/.*rc?(.{js,json})",
+        ],
+      },
+    ],
+  },
+};
