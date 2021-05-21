4da0917fb2a2410622cd74940ab4bc22dc10a64c
Philip Blyth <philip.blyth@autodesk.com>
2021-05-21 17:53:05 -0400

    fix: class conflict in comment lines?

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 const Comment: React.FC = ({ children }) => (
-  <div className="line">
+  <div>
     {`//`} {children}
   </div>
 );
@@ -9,7 +9,7 @@ const Comment: React.FC = ({ children }) => (
 const Comments: React.FC<{ lines: (string | React.ReactNode)[] }> = ({
   lines,
 }) => (
-  <p className="comments font-mono text-xs text-gray-500">
+  <p className="font-mono text-xs text-gray-500">
     {lines.map((line, index) => (
       <Comment key={index}>{line}</Comment>
     ))}
