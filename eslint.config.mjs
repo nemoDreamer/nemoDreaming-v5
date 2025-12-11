832e34df9e118889bf01b576a7e04ea5dcc9abfe
Philip Blyth <nemoDreamer@users.noreply.github.com>
2025-12-11 12:03:43 -0500

    fix: ESLint import order plug-in

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -17,6 +18,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+
+  {
+    plugins: {
+      import: importPlugin,
+    },
+  },
 
   // ADDITIONAL RULES:
   // --------------------------------------------------
